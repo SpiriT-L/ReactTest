9c64a3a7c47c8a73a482e1c2f943a1e7c2d4c7a6
LeonID <lmatskevich79@gmail.com>
2023-10-22 21:25:12 +0300

    fix: fix files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,32 +6,20 @@ import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('Renders hello world', () => {
-    // ARANGE
     render(<WrappedApp />);
-    // ACT
-    // const message = screen.queryByText(/Hello World/i);
-
-    // EXPECT
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
     ).toHaveTextContent('Hello World');
-    // expect(message).toBeVisible();
   });
   it('Renders hi', () => {
-    // ARANGE
     render(<WrappedApp />);
-    // ACT
-    // const message = screen.queryByText(/Hello World/i);
-
-    // EXPECT
     expect(
       screen.getByRole('heading', {
         level: 2,
       })
     ).toHaveTextContent('hi');
-    // expect(message).toBeVisible();
   });
 
   it('Renders not found if invalid path', () => {
